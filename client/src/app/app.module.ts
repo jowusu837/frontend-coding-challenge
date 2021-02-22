bf039934242eef2a16e5daef1ad92b7f6a0e53c2
Victor J. Owusu <jowusu837@gmail.com>
2021-02-22 13:04:17 +0000

    Rename app folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BackendService} from "../services/backend.service";
+import { SortableDirective } from './sortable.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortableDirective
   ],
   imports: [
     BrowserModule,
